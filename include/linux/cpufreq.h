058b1c21ee68664c6e1b9edf89f15a36ce6e6eaa
engstk <eng.stk@sapo.pt>
2014-12-15 13:40:54 +0000

    blu_active: dynamic cpufreq policy governor designed for latency-sensitive workloads
    Based from Google and CM Interactive
    Bits and patches from CAF and ondemand cpu governor
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -534,9 +534,15 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+<<<<<<< HEAD
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
+=======
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+>>>>>>> 0615c9a... blu_active: dynamic cpufreq policy governor designed for latency-sensitive workloads
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
