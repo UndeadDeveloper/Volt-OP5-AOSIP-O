9f8b65def6e4b26457a058e261654436e6816550
franciscofranco <franciscofranco.1990@gmail.com>
2017-06-26 09:12:19 +0200

    display: add a simple api to query the display state (on/off) at any.
    Signed-off-by: AndroidGX <subut3x@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -22,6 +22,7 @@
 #include <linux/qpnp/pwm.h>
 #include <linux/err.h>
 #include <linux/string.h>
+#include <linux/display_state.h>
 
 #include "mdss_dsi.h"
 #include "mdss_dba_utils.h"
@@ -41,6 +42,12 @@ bool is_display_on()
 }
 
 DEFINE_LED_TRIGGER(bl_led_trigger);
+
+bool display_on = true;
+bool is_display_on()
+{
+	return display_on;
+}
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
@@ -956,9 +963,16 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
 	}
+<<<<<<< HEAD
 
 	display_on = true;
 
+=======
+	
+	display_on = true;
+	
+    pr_err("%s start\n", __func__);
+>>>>>>> d2f71a1... display: add a simple api to query the display state (on/off) at any.
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1065,6 +1079,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dba_utils_video_off(pinfo->dba_data);
 		mdss_dba_utils_hdcp_enable(pinfo->dba_data, false);
 	}
+	
+	display_on = false;
 
 end:
 	pr_debug("%s:-\n", __func__);
